b78539d188c3f96be17efd3f43e548e4917f5b8c
Ryan Lubke <ryan.lubke@oracle.com>
2018-05-31 21:48:37 -0700

    Initial implementation of Optional support.

--- genson/src/main/java/com/owlike/genson/GensonBuilder.java
+++ genson/src/main/java/com/owlike/genson/GensonBuilder.java
@@ -951,6 +951,9 @@ public class GensonBuilder {
     converters.add(DefaultConverters.BigIntegerConverter.instance);
     converters.add(DefaultConverters.UUIDConverter.instance);
     converters.add(DefaultConverters.FileConverter.instance);
+    converters.add(DefaultConverters.OptionalIntConverter.instance);
+    converters.add(DefaultConverters.OptionalLongConverter.instance);
+    converters.add(DefaultConverters.OptionalDoubleConverter.instance);
     return converters;
   }
 
@@ -969,6 +972,7 @@ public class GensonBuilder {
     factories.add(new DefaultConverters.CalendarConverterFactory(
       new DefaultConverters.DateConverter(dateFormat, useDateAsTimestamp)
     ));
+    factories.add(DefaultConverters.OptionalConverterFactory.instance);
   }
 
   protected void addDefaultContextualFactories(List<ContextualFactory<?>> factories) {
