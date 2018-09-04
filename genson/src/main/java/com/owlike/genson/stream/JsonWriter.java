e10750ae73a23bf671002f87429a795f7ebf7563
Ryan Lubke <ryan.lubke@oracle.com>
2018-09-04 12:16:17 -0400

    Ignore skipNull configuration for JSON array types as null can be signifigant in that situation

--- genson/src/main/java/com/owlike/genson/stream/JsonWriter.java
+++ genson/src/main/java/com/owlike/genson/stream/JsonWriter.java
@@ -479,7 +479,7 @@ public class JsonWriter implements ObjectWriter {
   }
 
   public ObjectWriter writeNull() {
-    if (skipNull && _ctx.peek() != JsonType.ARRAY) {
+    if (skipNull && _ctx.peek() == JsonType.OBJECT) {
       _name = null;
     } else {
       beforeValue();
